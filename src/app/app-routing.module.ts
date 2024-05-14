ef8c82eff2d2843ec6e27fa3afd1c27aae9dc9e6
josue <josue@tec-software.com>
2024-05-13 23:07:07 -0300

    FIX, scroll top on navigate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,25 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { DashboardPortafolioComponent } from "./dashboard-portafolio/dashboard-portafolio.component";
-import { HomeComponent } from "./dashboard-portafolio/pages/home/home.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardPortafolioComponent } from './dashboard-portafolio/dashboard-portafolio.component';
+import { HomeComponent } from './dashboard-portafolio/pages/home/home.component';
 
-const routes : Routes =[
-
-      {
-        path: '',
-        loadChildren: ()=> import('./dashboard-portafolio/dashboard-portafolio.module').then((m)=>m.DashboardPortafolioModule)
-      },
-     {
-        path:'**',
-        redirectTo: 'home'
-      }, 
-]
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./dashboard-portafolio/dashboard-portafolio.module').then(
+        (m) => m.DashboardPortafolioModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
 @NgModule({
-imports:[RouterModule.forRoot(routes)],
-exports:[RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }), // Restaura siempre el scroll del objeto window al realizar navegacion
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule{}
+export class AppRoutingModule {}
