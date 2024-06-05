e67dbe6539fe6b01cbdeed11c90214ce9a80e868
evelinanunez <evenunez@alumno.unlam.edu.ar>
2024-06-04 23:18:22 -0300

    se arreglan un par de estilos a pagina technologies y se agredan dos card en portafolio

--- src/app/dashboard-portafolio/pages/portafolio/portafolio.component.ts
+++ src/app/dashboard-portafolio/pages/portafolio/portafolio.component.ts
@@ -7,18 +7,28 @@ import { Component } from '@angular/core';
 })
 export class PortafolioComponent {
 
-
+  isDisabled: boolean = true;
 
   proyectoCalculadoraEve() : void  {
     window.open('https://calculadora-evelina.netlify.app', '_blank');
   }
 
+  proyectoCalculadoraEveGitHub(): void {
+    window.open('https://github.com/evelinanunez/calculadora-evelina/tree/master', '_blank');
+  }
   proyectoCursoCoderHouseEve() : void  {
     window.open('https://proyecto-curso-angular-coderhouse.netlify.app', '_blank');
   }
 
+  proyectoCursoCoderHouseEveGithub():void{
+    window.open('https://github.com/evelinanunez/1PFNunez', '_blank');
+  }
   proyectoApiRick(): void  {
     window.open('https://api-rick-and-morthy.netlify.app', '_blank');
+  }
+
+  proyectoApiRickGithub() : void{
+    window.open('https://github.com/evelinanunez/ProyectoApiRestEvelina', '_blank');
   }
 
 }
